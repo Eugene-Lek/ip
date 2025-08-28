ab34f86e8c83dcaf5ae1d38947001a4fe508c67d
eugenelek <lekwceugene@gmail.com>
2025-08-28 20:25:49 +0800

    add: checkstyle

--- src/test/java/bot/TodoTest.java
+++ src/test/java/bot/TodoTest.java
@@ -11,7 +11,7 @@ public class TodoTest {
     @Test
     public void TestTodoWithoutDeadlineToDBRepresentation() {
         Todo todo = new Todo("Item &*@^$@% HJSKF HIU*(#@34", null);
-        String actual = todo.toDBRepresentation();
+        String actual = todo.toDbRepresentation();
         String expected = "T|false|Item &*@^$@% HJSKF HIU*(#@34";
         assertEquals(actual, expected);
     }
@@ -20,7 +20,7 @@ public class TodoTest {
     public void TestTodoWithDeadlineToDBRepresentation() {
         Todo todo = new Todo("fkjaewhr erkj2n 3 '3498p ad  c uwthfrsd", LocalDate.of(2001, Month.MARCH, 12).atStartOfDay());
         todo.markAsCompleted();
-        String actual = todo.toDBRepresentation();
+        String actual = todo.toDbRepresentation();
         String expected = "D|true|fkjaewhr erkj2n 3 '3498p ad  c uwthfrsd|2001-03-12T00:00";
         assertEquals(actual, expected);
     }
